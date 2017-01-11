20a9146f0a44024d6a4577dd38031e03f36b273b
gediminas1994 <gediminas.zinkevicius@gmail.com>
2017-01-11 16:45:07 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Item;
+use Illuminate\Http\Request;
 use Input;
 
-class QueryController extends Controller{
-	
-    public function search(Request $request){
+class QueryController extends Controller
+{
+    public function search(Request $request)
+    {
         //$query = Request::input('search');
         $query = Input::get('search');
-        $items = new Item;
-        $items = Item::where('title', 'LIKE', '%' . $query . '%')->paginate(6);  
-        
+        $items = new Item();
+        $items = Item::where('title', 'LIKE', '%'.$query.'%')->paginate(6);
+
         return view('items.searchItem')->with('items', $items)->with('query', $query);
     }
 }
--- app/Item.php
+++ app/Item.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-   protected $table = 'items';
+    protected $table = 'items';
 
     public function user()
     {
